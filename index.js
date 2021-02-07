acf7a208fc5d3e444f37287de707f207890e6ecf
Lalla Sankara <lallasankara@gmail.com>
2021-02-07 00:07:28 +0000

    react client update

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const { google } = require('googleapis')
 
 const { OAuth2 } = google.auth
 
-const oAuth2Client = new OAuth2('725875135322-ntvidf9jg41l9onofhqo593poaj22p6m.apps.googleusercontent.com','8Xk0mAg8cyEXQIIPa-JNBIo8'
+const oAuth2Client = new OAuth2('725875135322-ntvidf9jg41l9onofhqo593poaj22p6m.apps.googleusercontent.com', '8Xk0mAg8cyEXQIIPa-JNBIo8'
 )
 
-oAuth2Client.setCredentials({refresh_token: '1//04tbg88IcQwP6CgYIARAAGAQSNwF-L9IrfiSiGsVX1dECNHuA1QJcJ0vachpvhjPrhHyrameHpQJbly001jWyY3OZVN7Z5mmuVuA',
+oAuth2Client.setCredentials({
+  refresh_token: '1//04tbg88IcQwP6CgYIARAAGAQSNwF-L9IrfiSiGsVX1dECNHuA1QJcJ0vachpvhjPrhHyrameHpQJbly001jWyY3OZVN7Z5mmuVuA',
 })
 
 const calendar = google.calendar({ version: 'v3', auth: oAuth2Client })
@@ -25,30 +26,30 @@ const event = {
   location: '515 Malcolm X Boulevard (135th St and, Malcolm X Blvd, New York, NY 10037',
   description: 'Daily Reminder to do task.',
   colorId: 1,
-  start: { 
-   dateTime: eventStartTime,
-   timeZone: 'America/New York',
-},
- end: {
+  start: {
+    dateTime: eventStartTime,
+    timeZone: 'America/New York',
+  },
+  end: {
     dateTime: eventEndTime,
     timeZone: 'America/New York',
 
- },
- 
+  },
+
 }
 
 
-calendar.freebusy.query( 
-{
-  resource:{
-  timeMin: eventStartTime, 
-  timeMax: eventEndTime,
-  timeZone: 'America/New York',
-  items: [{ id: 'primary' }],
+calendar.freebusy.query(
+  {
+    resource: {
+      timeMin: eventStartTime,
+      timeMax: eventEndTime,
+      timeZone: 'America/New York',
+      items: [{ id: 'primary' }],
+    },
   },
-},
- (err, res) => {
-    if(err) return console.error('Free Busy Query Error: ', err)
+  (err, res) => {
+    if (err) return console.error('Free Busy Query Error: ', err)
 
     const eventArr = res.data.calendars.primary.busy
 
@@ -56,7 +57,7 @@ calendar.freebusy.query(
     if (eventArr.length === 0)
       // If we are not busy create a new calendar event.
       return calendar.events.insert(
-        { calendarId: 'primary', resource: event }, 
+        { calendarId: 'primary', resource: event },
         err => {
           // Check for errors and log them if they exist.
           if (err) return console.error('Error Creating Calender Event:', err)
@@ -68,5 +69,5 @@ calendar.freebusy.query(
     // If event array is not empty log that we are busy.
     return console.log(`Sorry I'm busy...`)
 
-}
+  }
 )
